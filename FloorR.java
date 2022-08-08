b036d0d385ced229f433fae0ccd282b78eb0f908
Aashishrj <97225718+Aashishrj@users.noreply.github.com>
2022-08-08 14:04:02 +0530

    Create FloorR.java

---
+++ FloorR.java
@@ -0,0 +1,29 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+      int arr[]={2,3,5,9,14,16,18};
+      int target=15;
+      int v=floor(arr,target);
+      System.out.println(v);
+    }
+    public static int floor(int arr[],int target)
+    {
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(arr[mid]>target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return high;
+    }
+}
