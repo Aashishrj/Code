f5d8bf04fcc4f472726b89959c91517d21b129ec
Aashishrj <97225718+Aashishrj@users.noreply.github.com>
2022-09-16 11:14:45 +0530

    Create constructorinitialize.java

---
+++ constructorinitialize.java
@@ -0,0 +1,23 @@
+class test
+{
+    int a;
+    String s;
+    test(int a,String s)
+    {
+        this.a=a;
+        this.s=s;
+        System.out.println("this is pass by argument"+a+" "+s);
+    }
+    test()
+    {
+        System.out.println("not pass by argument");
+    }
+}
+class HelloWorld {
+    public static void main(String[] args) {
+        // test obj=new test(10,"raju");
+        test obj=new test();
+        
+        
+    }
+}
